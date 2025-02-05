7f5c2880fb20e37d88f0450fa0376e7cb70adbe0
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2025-02-05 11:19:02 -0300

    Tentei mudar a lógica do posicionamento, mas continua com o mesmo problema, e ainda quebrou o estilo

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 const searchInput = document.getElementById('search-input')
 const resultsArtist = document.getElementById('result-artist')
+const gridCont = document.getElementById('grid-container')
 const resultPlaylist = document.getElementById('result-playlists')
 
 
 // Busca resultados da pesquisa na api
 function requestApi(searchValue) {
-    const url = `http://localhost:3000/artists?name_like=${searchValue}`
+    const url = `http://localhost:3000/artists?name_like=${searchValue}`//ta dando erro
     fetch(url)  // Busca dados na api
-        .then((response) => response.json()) // Retorna uma resposta (especificado em json)
-        .then((results) => displayResults(results)) // Results é um objeto json com a resposta da busca
-        
+        .then((response) => response.json()) // Retorna uma resposta em json
+        .then((results) => displayResults(results)) // Results é um objeto json com a resposta da busca      
 };
 
 
 // Mostra os resultados e oculta as playlists
 function displayResults(results) {
     resultPlaylist.classList.add('hidden')  // Oculta as playlists
-    
-    const artistName = document.getElementById('artist-name')   // Define constantes para os dados da api
-    const artistImage = document.getElementById('artist-img')   //
 
-    results.forEach((element) => {          // NA TEORIA mostra os resultados da pesquisa com filtro, NA TEORIA 
-    artistName.innerText = element.name     // 
-    artistImage.src = element.urlImg        //
-    });                                     //
+    results.forEach((element) => {  
     
-
-    // Precisa criar elementos dinâmicamente, caso contrário irá mostrar apenas um ultimo dado
-    results.forEach(element => {
+        
+        // Cria um elemento para o texto do card
+        const artistName = document.getElementById('artist-name')
+        artistName.classList.add('card-text')
         artistName.innerText = element.name
-        artistImage.src = element.urlImg
-    });
+
+        // Cria um elemento para a imagem do card
+        const artistImage = document.getElementById('artist-img')
+        artistImage.classList.add('artist-img')
+        artistImage.src = element.urlImg;
+
+        // Posiciona os elementos
+        gridCont.appendChild(artistName)
+        gridCont.appendChild(artistImage)
+    });                                     
+    
 
     resultsArtist.classList.remove('hidden')
 }
@@ -38,13 +42,14 @@ function displayResults(results) {
 document.addEventListener('input', function() {
     const searchValue = searchInput.value.toLowerCase()
     if (searchInput.value === '') {
-        resultPlaylist.classList.add('hidden')
-        resultsArtist.classList.remove('hidden')
+        resultPlaylist.classList.remove('hidden')
+        resultsArtist.classList.add('hidden')
         return;
     } 
 
     requestApi(searchValue)
 });
+
 
 
 // Mostra um cumprimento pelo horário do dia
