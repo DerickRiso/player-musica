cd3c609b69bdee912c1b2996870a30eba989fc6f
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2025-02-06 12:03:47 -0300

    Testando possibilidade de uma refatoração da pesquisa

---
+++ script1.js
@@ -0,0 +1,76 @@
+const searchInput = document.getElementById('search-input')
+const resultsArtist = document.getElementById('result-artist')
+const gridCont = document.getElementById('grid-container')
+const resultPlaylist = document.getElementById('result-playlists')
+const searchValue = searchInput.value
+
+// Eu não sei como, eu não sei porque, mas isso funcionou de alguma forma, já é um avanço
+async function requestApi(searchValue) {
+    const response = await fetch("http://localhost:3000/artists");
+
+    const dados = await response.json();
+    const regex = new RegExp(searchValue);
+    const resultado = dados.filter(item => regex.test(item.id));
+
+    return resultado;
+};
+
+
+// Mostra os resultados e oculta as playlists
+function displayResults(results) {
+    resultPlaylist.classList.add('hidden')  // Oculta as playlists
+
+    results.forEach((element) => {  
+    
+        
+        // Cria um elemento para o texto do card
+        const artistName = document.getElementById('artist-name')
+        artistName.classList.add('card-text')
+        artistName.innerText = element.name
+
+        // Cria um elemento para a imagem do card
+        const artistImage = document.getElementById('artist-img')
+        artistImage.classList.add('artist-img')
+        artistImage.src = element.urlImg;
+
+        // Posiciona os elementos
+        gridCont.appendChild(artistName)
+        gridCont.appendChild(artistImage)
+    });                                     
+    
+
+    resultsArtist.classList.remove('hidden')
+}
+
+document.addEventListener('input', function() {
+    const searchValue = searchInput.value.toLowerCase()
+    if (searchInput.value === '') {
+        resultPlaylist.classList.remove('hidden')
+        resultsArtist.classList.add('hidden')
+        return;
+    } 
+
+    requestApi(searchValue).then(console.log);
+});
+
+
+
+// Mostra um cumprimento pelo horário do dia
+function cumprimentar() {
+    var res = document.getElementById('hello')
+    var data = new Date()
+    var hora = data.getHours()
+    var cumprimento
+
+    if (hora <= 4) {
+        cumprimento = "Boa madrugada"
+    } else if (hora >= 5 && hora < 12) {
+        cumprimento = "Bom dia"
+    } else if (hora >= 12 && hora < 18) {
+        cumprimento = "Boa tarde"
+    } else if (hora >= 18) {
+        cumprimento = "Boa noite"
+    }
+
+    res.innerHTML = `${cumprimento}`
+};
