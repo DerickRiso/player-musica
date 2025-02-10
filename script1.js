47ac46fa1ed0dba945befad1dc67c74274203fdc
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2025-02-10 10:17:37 -0300

    Bug de exibição de múltiplos resultados resolvido

--- script1.js
+++ script1.js
@@ -1,7 +1,6 @@
 const searchInput = document.getElementById('search-input')
 const resultsArtist = document.getElementById('result-artist')
 const gridCont = document.getElementById('grid-container')
-const cardArtist = document.getElementById('artist-card')
 const resultPlaylist = document.getElementById('result-playlists')
 const searchValue = searchInput.value
 
@@ -12,9 +11,9 @@ async function requestApi(searchValue) {
     const dados = await response.json();
     
     // Cria uma expressão regular para a entrada do usuário
-    const regex = new RegExp(searchValue);
+    const regex = new RegExp(searchValue, 'i');
     // Filtra os dados de acordo com a entrada
-    const resultado = dados.filter(item => regex.test(item.name.toLowerCase()));
+    const resultado = dados.filter(item => regex.test(item.name));
     // Chamada à função para exibição dos resultados
     displayResults(resultado);
     return resultado;
@@ -24,18 +23,42 @@ async function requestApi(searchValue) {
 // Mostra os resultados e oculta as playlists
 function displayResults(resultado) {
     resultPlaylist.classList.add('hidden')  // Oculta as playlists
+    gridCont.innerHTML = '';
 
     resultado.forEach((element) => {  
+        // Cria o card
+        const cardArtist = document.createElement('div')
+        cardArtist.classList.add('artist-card')
+
+        // Cria o container da imagem
+        const cardImg = document.createElement('div')
+        cardImg.classList.add('card-img')
+
+        // Cria o container do texto
+        const cardText = document.createElement('div')
+        cardText.classList.add('card-text')
+
+        // Cria a categoria do card
+        const categorie = document.createElement('span')
+        categorie.classList.add('artist-categorie')
+        categorie.innerText = "Artista"
     
-        // Cria um elemento para o texto do card
-        const artistName = document.getElementById('artist-name')
+        // Cria o texto do card
+        const artistName = document.createElement('span')
         artistName.classList.add('artist-name')
         artistName.innerText = element.name
 
-        // Cria um elemento para a imagem do card
-        const artistImage = document.getElementById('artist-img')
+        // Cria a imagem do card
+        const artistImage = document.createElement('img')
         artistImage.classList.add('artist-img')
         artistImage.src = element.urlImg;
+
+
+        cardArtist.appendChild(cardImg)
+        cardArtist.appendChild(cardText)
+        cardImg.appendChild(artistImage)
+        cardText.appendChild(artistName)
+        cardText.appendChild(categorie)
 
         // Posiciona os elementos
         gridCont.appendChild(cardArtist);
